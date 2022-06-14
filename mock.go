1520e9de3e87460b0ad12b1f8f0d13bce1b98bb8
matias-ziliotto <matias.ziliotto@mercadolibre.com>
2022-06-14 19:41:27 -0300

    use absolute path

--- mock.go
+++ mock.go
@@ -3,10 +3,15 @@ package mock
 import (
 	"encoding/json"
 	"io/ioutil"
+	"log"
+	"path/filepath"
 )
 
 func ReadJsonFile(filePath string, data interface{}) error {
-	fileData, err := ioutil.ReadFile("/tests/mocks/" + filePath + ".json")
+	absPath, err := filepath.Abs("../../tests/mocks/" + filePath + ".json")
+	log.Println("Path:", err)
+	log.Println("Path:", absPath)
+	fileData, err := ioutil.ReadFile(absPath)
 
 	if err != nil {
 		return err
